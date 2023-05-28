4326fb6fe8ebd7d982370cd4047e4762f780a0d0
Altka <altankhuu0211@gmail.com>
2023-05-28 23:45:13 +0800

    analytics service

--- src/services/index.ts
+++ src/services/index.ts
@@ -33,38 +33,13 @@ export const getSemesterWeek = () => {
   })
 }
 
-// export const getClassAttendanceAnalytic = () => {
-//   return {
-//     data: {
-//       total_free: [2, 0, 1, 0, 3, 1, 2, 0, 5], //1-16-р долоо хоног хүртэлх
-//       total_sick: [0, 1, 1, 2, 0, 0, 0, 3, 0],
-//       total_absent: [1, 2, 3, 0, 0, 0, 5, 7, 0],
-//       total_present: [50, 50, 48, 51, 50, 52, 46, 43, 47],
-//     },
-//   }
-// }
-
-// export const getLessonSchedule = (params: LessonSchedule) => {
-//   return api({
-//     url: URI.GET_LESSON_LIST,
-//     params: { teacher_id: params.teacher_id },
-//   })
-// }
-
-// export const getPar = (usercode) => {
-//   const data = await api.service("teacher_lesson").find({ query: { user_code: usercode }});
-
-//   data = [
-//     { user_code: "J.SW010", lesson_code: "F.CS310" },
-//       { user_code: "J.SW010", lesson_code: "F.CS310" },
-//         { user_code: "J.SW010", lesson_code: "F.CS310" },
-//           { user_code: "J.SW010", lesson_code: "F.CS310" },
-//   ];
-
-//   const lesson_names = await api.service("lessons").find({ query: { $in: data.map(item => { return data.lesson_code }) }})
-
-//   return lessons_data;
-// }
+export const getAnalytics = (params) => {
+  return api({
+    // url: URI.GET_SEMESTER_WEEK
+    url: `${root}/analytics`,
+    params: params,
+  })
+}
 
 export const getLessonSchedule = (params) => {
   return api({
